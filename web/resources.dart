85dad77d3c101b31eaae34ceddfb5ee5b8f00393
Andreas Säuberli <aandivb@gmail.com>
2015-08-22 20:25:15 +0200

    resource loading fixed

--- web/resources.dart
+++ web/resources.dart
@@ -1,9 +1,10 @@
 part of ld33;
 
-Map<String, ImageElement> image;
+Map<String, ImageElement> images;
 Map<String, AudioElement> sounds;
 
 void loadResources() {
   images = new Map<String, ImageElement>();
+  //images['test'] = new ImageElement(src: 'res/start.png');
   sounds = new Map<String, AudioElement>();
 }
